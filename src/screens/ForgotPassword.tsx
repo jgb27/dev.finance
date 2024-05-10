842a7f5dd739552f9cbaf15cf6f84d682a572c36
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-05-10 12:06:01 -0300

    refactor: Add ForgotPassword screen component

--- src/screens/ForgotPassword.tsx
+++ src/screens/ForgotPassword.tsx
@@ -1,6 +1,66 @@
+import { Container, Flex, FormControl, Text } from "@chakra-ui/react";
+import Title from "../components/Title";
+import FormInput from "../components/FormInput";
+import ButtonSubmit from "../components/ButtonSubmit";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 const ForgotPassword = () => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        email: ""
+    });
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
     return (
-        <h1>Home</h1>
+        <Container maxW='md' boxShadow={'lg'} padding={12} mt={22}>
+            <Flex direction='column' gap={8}>
+                <Title color="primary" font="4xl" fontFamily="poppins" />
+                <Flex gap={4} direction='column'>
+                    <Text fontFamily="inter" fontSize="24px" fontWeight="600" align='center' color="text">
+                        Forgot Password
+                    </Text>
+                    <Text fontFamily="poppins" fontSize="16px" fontWeight="300" align='center' color="gray.2">
+                        Enter you email address to get the password reset link.
+                    </Text>
+                    <FormControl>
+                        <Flex gap={4} direction='column'>
+                            <FormInput
+                                font="poppins"
+                                type='email'
+                                name='email'
+                                onChange={handleInputChange}
+                                formData={formData}
+                            />
+                            <Flex justify='center' direction='column' gap={1}>
+                                <ButtonSubmit
+                                    bg="primary"
+                                    color="white"
+                                    text="Password Reset"
+                                    onSubmit={() => {
+                                        alert("Password reset link sent to your email")
+                                        navigate('/authentication')
+                                    }}
+                                />
+                                <ButtonSubmit
+                                    bg="white"
+                                    color="gray.2"
+                                    text="Back to login"
+                                    onSubmit={() => navigate('/authentication')}
+                                />
+                            </Flex>
+                        </Flex>
+                    </FormControl>
+                </Flex>
+            </Flex>
+        </Container >
     )
 }
 
