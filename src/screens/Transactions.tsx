3cd372bc9da414bd878197cf18724c08cfae0110
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-05-14 12:30:13 -0300

    refactor: Update router configuration to include new screens
    - Added new screens for Overview, Transactions, Bills, and Expense
    - Updated router configuration to include new paths and corresponding components

---
+++ src/screens/Transactions.tsx
@@ -0,0 +1,14 @@
+import { Container } from "@chakra-ui/react";
+import Layout from "../Layout/LayoutApp"
+
+const Transactions = () => {
+    return (
+        <Layout title="Transactions">
+            <Container>
+                <h1>Transactions</h1>
+            </Container>
+        </Layout>
+    );
+};
+
+export default Transactions;
