706072ce4924f1b9774c1ffecdc76009000e6069
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-05-14 09:43:41 -0300

    feat: Add NavBar component to layout

---
+++ src/Layout/Navbar.tsx
@@ -0,0 +1,9 @@
+import Title from "../components/Title"
+
+const NavBar = () => {
+    return (
+        <Title color="white" fontSize="24" fontFamily="poppins" />
+    )
+}  
+
+export default NavBar
